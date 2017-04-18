bf7ca5731d9d2295da766c26c1c3fc44414c834d
LiXuFei <xfli@thoughtworks.com>
2017-04-18 08:24:21 +0800

    init RN pro.

---
+++ index.ios.js
@@ -0,0 +1,52 @@
+var MOCKED_MOVIES_DATA = [
+  { title: '标题', year: '2015'},
+];
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+} from 'react-native';
+
+import meIcon from './app/images/me.jpeg';
+
+export default class AwesomeProject extends Component {
+  render() {
+    const movie = MOCKED_MOVIES_DATA[0];
+    return (
+      <View style={styles.container}>
+        <Image source={meIcon} style={styles.thumbnail} />
+        <Text style={styles.centerText}>center text</Text>
+        <View style={styles.title}>
+          <Text>{movie.title}</Text>
+          <Text>{movie.year}</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
+  title: {
+    flex: 1,
+  },
+  centerText: {
+    flex: 1,
+  },
+});
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
